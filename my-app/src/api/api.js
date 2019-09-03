f54a244460a68105925acd7c073f79bc0b8a5720
ваше имяwlad999 <wladmail25@gmeil.com>
2019-09-03 13:45:49 +0300

    Updates

--- my-app/src/api/api.js
+++
@@ -1,6 +0,0 @@
-const baseUrl = "https://jsonplaceholder.typicode.com/";
-
-export const url = {
-  posts: () => `${baseUrl}posts/`,
-  selectedComents: id => `${baseUrl}comments?postId=${id}`
-};
---
+++ precache-manifest.723954a7439646ed6a7751e4c5de9fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1818bdcefcb398c245e9f28da1ca906b",
+    "url": "/wlad999/Test/tree/master/my-app/index.html"
+  },
+  {
+    "revision": "14e6f79d78af2ca22f7d",
+    "url": "/wlad999/Test/tree/master/my-app/static/css/main.dcf86726.chunk.css"
+  },
+  {
+    "revision": "2e076dca7535233b9d08",
+    "url": "/wlad999/Test/tree/master/my-app/static/js/2.649fcc0e.chunk.js"
+  },
+  {
+    "revision": "14e6f79d78af2ca22f7d",
+    "url": "/wlad999/Test/tree/master/my-app/static/js/main.f850663a.chunk.js"
+  },
+  {
+    "revision": "07101465299646f0c84e",
+    "url": "/wlad999/Test/tree/master/my-app/static/js/runtime~main.cd4b0a61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wlad999/Test/tree/master/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
